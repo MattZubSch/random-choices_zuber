82636fa5e3157ec5e4d33c228f1524b14679a7af
Matias Zuber Schachner <matias.zs.1967@gmail.com>
2023-01-18 23:05:00 -0300

    Trabajo con Core Components y StyleSheets

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Item } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>¡Hola, Coder!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View 
+        style={styles.inputContainer}
+        >
+        <TextInput placeholder='Item de la Lista'
+        style={styles.inputContainer}
+        />
+        <Button title='+'/>
+      </View>
+      <View style={styles.listContainer}> 
+      <Button title='Name'></Button>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+  },
+  input: {
+    borderBottomColor: 'black', 
+      borderBottomWidth: 1,
+      width: 200
+  },
+  listContainer: {
+    padding: 60,
+    marginTop: 10,
+    borderColor: 'black',
+    borderWidth: 1
+  }
+
 });
+
